5d18d12f384fba3f571616b7ca70b3f4cbd151ab
Bimpe1 <adebimpe.adetoba@pau.edu.ng>
2023-04-13 20:00:19 +0100

    Comment and variable

---
+++ Variable/index.js
@@ -0,0 +1,9 @@
+let myAge ;
+var myName;
+myName="Bimpe";
+myAge=18;
+var myHeight="5ft 8inches";
+myHeight=5.8;
+console.log("My name is "+ myName);
+console.log("My age is "+ myAge);
+console.log("My height is "+ myHeight);
