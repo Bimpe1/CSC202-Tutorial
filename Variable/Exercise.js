f36ba844afdcdbae926a3c084aeedcb392388f5d
Bimpe1 <adebimpe.adetoba@pau.edu.ng>
2023-04-16 16:33:48 +0100

    Madlibs game

--- Variable/Exercise.js
+++ Variable/Exercise.js
@@ -1,1 +1,16 @@
 //Create a madlibs game using variables
+console.log("Welcome to my Madlibs game.");
+console.log("--------------------------");
+let Verb1 = "Twinkle";
+let Adjective1 = "little";
+let Noun1 = "star";
+let Noun2 = "I";
+let Noun3="you";
+let Preposition1 ="above";
+let Adjective2 = "high";
+let Noun4 = "diamond";
+
+console.log(Verb1 + " twinkle "+ Adjective1 + " " + Noun1 + ",")
+console.log("How " + Noun2 +" wonder what " + Noun3 + " are, ")
+console.log("Up " + Preposition1 + " the world so " + Adjective2 + ",")
+console.log("Like a " + Noun4 + " in the sky.")
